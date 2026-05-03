44ac0dab9fc3c80b090f6269862ea854c57f66f3
moonglow2003 <moonglow200304@gmail.com>
2026-05-03 10:49:54 +0530

    fix: prisma export issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,9 +4,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
+export default prisma;
